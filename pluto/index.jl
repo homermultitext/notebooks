c4d1a1f59e7fce459d381e9cea03eb267a8b6343
Neel Smith <dnsmith.neel@gmail.com>
2022-07-29 18:46:03 -0400

    Initial version of index page.

--- pluto/index.jl
+++ pluto/index.jl
@@ -15,10 +15,18 @@ Built with Pluto!
 
 # ╔═╡ c9249cee-80f2-4335-bea6-b2ac4c62ad99
 md"""
-Links when running in PlutoSliderServer:
+Links when running locally in PlutoSliderServer:
 
 -  [lightbox](./lightbox.html?isoloated_cell_id=1e65d74b-add2-4b53-b382-f178fd20dbd7)
 
+
+To be added:
+
+- alpha-search
+- codex-browser
+- iliad-browser
+- authlists
+- thermometer
 
 """
 
@@ -38,7 +46,7 @@ manifest_format = "2.0"
 """
 
 # ╔═╡ Cell order:
-# ╠═b289c72c-0f8e-11ed-03f3-cde38f56ec48
-# ╠═c9249cee-80f2-4335-bea6-b2ac4c62ad99
+# ╟─b289c72c-0f8e-11ed-03f3-cde38f56ec48
+# ╟─c9249cee-80f2-4335-bea6-b2ac4c62ad99
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
