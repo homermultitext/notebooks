0a25276f03a2f6bc716799d059d84e080f794711
Neel Smith <dnsmith.neel@gmail.com>
2022-07-29 18:44:01 -0400

    Add index.jl

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Pluto notebooks for working with published data from the HMT project.
 
 
 Lightbox.jl: the dashboard UI is in cell `1e65d74b-add2-4b53-b382-f178fd20dbd7`
+
+So this should work for running in PlutoSliderServer locally:
+
+
+`http://localhost:2345/lightbox.html?isolated_cell_id=1e65d74b-add2-4b53-b382-f178fd20dbd7`
---
+++ pluto/index.jl
@@ -0,0 +1,44 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ b289c72c-0f8e-11ed-03f3-cde38f56ec48
+md"""
+# HMT dashboards
+
+Built with Pluto!
+
+
+"""
+
+# ╔═╡ c9249cee-80f2-4335-bea6-b2ac4c62ad99
+md"""
+Links when running in PlutoSliderServer:
+
+-  [lightbox](./lightbox.html?isoloated_cell_id=1e65d74b-add2-4b53-b382-f178fd20dbd7)
+
+
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.3"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═b289c72c-0f8e-11ed-03f3-cde38f56ec48
+# ╠═c9249cee-80f2-4335-bea6-b2ac4c62ad99
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
